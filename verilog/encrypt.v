5b39a8ed09ded7ec694460a72fc12d5d7097844c
J.D. Kelly <jdkelly@iron-502-26.farmshare.sunet>
2022-04-06 19:21:05 +0000

    functional decrypt, working on encrypt

--- verilog/encrypt.v
+++ verilog/encrypt.v
@@ -1,10 +1,11 @@
 module encrypt
 #(
-    parameter PLAINTEXT_WIDTH = 8,          // log2(p)
+    parameter PLAINTEXT_MODULUS = 64,
+    parameter PLAINTEXT_WIDTH = 6,
+    parameter DIMENSION = 1,
+    parameter CIPHERTEXT_MODULUS = 1024,
     parameter CIPHERTEXT_WIDTH = 10,
-    parameter CIPHERTEXT_MODULUS = 1024,    // q
-    parameter BIG_N = 30,                   // N
-    parameter LITTLE_N = 2,                 // n
+    parameter BIG_N = 30
 )
 (
     input clk,
@@ -14,25 +15,25 @@ module encrypt
     input [PLAINTEXT_WIDTH-1:0] plaintext,
     input [CIPHERTEXT_WIDTH-1:0] publickey_row [BIG_N-1:0],
     input [BIG_N-1:0] noise_select,
-    input [LITTLE_N-1:0] row,
+    input [DIMENSION:0] row,
 
     // note: outputs partial over time
-    output wire [CIPHER_TEXT_WIDTH-1:0] ciphertext,
+    output wire [CIPHER_TEXT_WIDTH-1:0] ciphertext
 );
     wire [CIPHERTEXT_WIDTH-1:0] psum [2*BIG_N-1:0];
+    genvar n, i;
     generate
-        genvar p = 0;
-        for (int n = BIG_N; n > 1; n >> 1) begin
-            for (int i = 0; i < n; i+=1) begin
+        int p = 0;
+        for (n = BIG_N; n > 1; n >> 1) begin: small_n_loop
+            for (i = 0; i < n; i=i+1) begin: i_loop
                 if (n == BIG_N) begin
-                    psum[i] = (noise_select[i]? publickey_row[i] : BIG_N'b0) + (noise_select[i+1]? publickey_row[i+1] : BIG_N'b0);
+                    assign psum[i] = (noise_select[i]? publickey_row[i] : BIG_N'b0) + (noise_select[i+1]? publickey_row[i+1] : BIG_N'b0);
                 else begin
-                    psum[p+i] = psum[p-(2*n)+i] + psum[p-(2*n)+i+1];
+                    assign psum[p+i] = psum[p-(2*n)+i] + psum[p-(2*n)+i+1];
                 end
             end
             p = p + n;
         end
-        assign ciphertext = psum[p];
     endgenerate
-
+    assign ciphertext = psum[p];
 endmodule
