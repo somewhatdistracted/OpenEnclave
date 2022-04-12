ac75ad5b50edbcb79f7f0c27e4d892fc36a56046
Ian MacFarlane <ian@macfarlanehome.com>
2022-04-12 14:56:30 -0700

    changes

--- verilog/encrypt.v
+++ verilog/encrypt.v
@@ -2,16 +2,15 @@ module encrypt
 #(
     parameter PLAINTEXT_MODULUS = 64,
     parameter PLAINTEXT_WIDTH = 6,
-    parameter DIMENSION = 1,
     parameter CIPHERTEXT_MODULUS = 1024,
     parameter CIPHERTEXT_WIDTH = 10,
-    parameter BIG_N = 30
+    parameter DIMENSION = 10,
+    parameter BIG_N = 30,
 )
 (
     input clk,
     input rst_n,
 
-    input go,
     input [PLAINTEXT_WIDTH-1:0] plaintext,
     input [CIPHERTEXT_WIDTH-1:0] publickey_row [BIG_N-1:0],
     input [BIG_N-1:0] noise_select,
@@ -20,19 +19,12 @@ module encrypt
     // note: outputs partial over time
     output wire [CIPHERTEXT_WIDTH-1:0] ciphertext
 );
-    wire [CIPHERTEXT_WIDTH-1:0] psum [2*BIG_N-1:0];
-    genvar n, i;
+    wire [CIPHERTEXT_WIDTH-1:0] psum [BIG_N-1:0];
     generate
-        p = 0;
-        for (n = BIG_N; n > 1; n=n>>1) begin: small_n_loop
-            for (i = 0; i < n; i=i+1) begin: i_loop
-                if (n == BIG_N) begin
-                    assign psum[i] = (noise_select[i]? publickey_row[i] : {BIG_N{1'b0}}) + (noise_select[i+1]? publickey_row[i+1] : {BIG_N{1'b0}});
-                end else begin
-                    assign psum[p+i] = psum[p-(2*n)+i] + psum[p-(2*n)+i+1];
-                end
-            end
-            int p = p + n;
+        genvar i;
+        assign psum[0] = ((row == 1)? plaintext : 0) + (noise_select[0]? publickey_row[i] : 0);
+        for (i = 1; i < BIG_N; i=i+1) begin: i_loop
+            psum[i] = psum[i-1] + (noise_select[i]? publickey_row[i] : 0);
         end
     endgenerate
     assign ciphertext = psum[p];
